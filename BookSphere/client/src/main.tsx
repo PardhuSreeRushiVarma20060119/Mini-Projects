39e0797a8ab03eabf50ed2f179be5ccfe42d386f
PardhuSreeRushiVarma20060119 <pardhusreerushivarma@gmail.com>
2026-03-04 19:43:08 +0530

    chore: remove BookSphere mini-project and all its associated files.

--- BookSphere/client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Import Merriweather, Open Sans, and Playfair Display from Google Fonts
-// (This would normally be in the HTML file, but we're including it here for completeness)
-
-createRoot(document.getElementById("root")!).render(<App />);
